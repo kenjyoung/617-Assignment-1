85db7e2f5fd9751e819cd1d38e246d3634085103
Mike <kjyoung@ualberta.ca>
2016-02-01 11:32:25 -0700

    minor change

--- TangentVectors.m
+++ TangentVectors.m
@@ -10,11 +10,11 @@ for i=1:num_images
     [X,Y] = meshgrid(-c(1):size(image,1)-c(1)-1,-c(2):size(image,2)-c(2)-1);
     [dx, dy] = imgradientxy(image);
 
-    drot = [dx.*Y-dy.*X];
+    drot = dx.*Y-dy.*X;
 
-    dscale = [dx.*X+dy.*Y];
+    dscale = dx.*X+dy.*Y;
 
-    daspect = [dx.*X-dy.*Y];
+    daspect = dx.*X-dy.*Y;
 
     dshear = dx.*Y;
 
