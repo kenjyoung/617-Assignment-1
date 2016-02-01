8ac428001891a2be09ea0c4c624986e480eb10f3
Mike <kjyoung@ualberta.ca>
2016-02-01 12:03:22 -0700

    tried implementing tangent distance, need to debug

--- tangentDistance.m
+++ tangentDistance.m
@@ -1,30 +1,17 @@
-function [ dist ] = tangentDistance( image1 , tangents1, image2, tangents2)
+function [ dist ] = tangentDistance( image1 , M1, image2, M2)
 %TANGENTDISTANCE compute the tangent distance between two input images
-assert(size(image1) == size(image2), 'image sizes must be equal to compute tangent distance');
-c1 = size(image1,1)/2
-c2 = size(image1,2)/2
-[X,Y] = meshgrid(-c1:size(image1,1)-c1,-c2:size(image1,2)-c2)
-[dx1, dy1] = imagegradientxy(image1);
-[dx2, dy2] = imagegradientxy(image2);
+assert(isequal(size(image1),size(image2)), 'image sizes must be equal to compute tangent distance');
+I1 = image1(:);
+I2 = image2(:);
 
-drot1 = [dx1,dy1].*[Y,-X]
-drot2 = [dx2,dy2].*[Y,-X]
+size([M1'*M1,-M1'*M2; M2'*M1,-M2'*M2])
+size([M1'*(I2-I1); M2'*(I2-I1)])
 
-dscale1 = [dx1,dy1].*[X,Y]
-dscale2 = [dx2,dy2].*[X,Y]
+u = [M1'*M1,-M1'*M2; M2'*M1,-M2'*M2]\[M1'*(I2-I1); M2'*(I2-I1)];
+u1 = u(1:size(u)/2);
+u2 = u(size(u)/2+1:end);
 
-daspect1 = [dx1, dy1].*[X,-Y]
-daspect2 = [dx2, dy2].*[X,-Y]
-
-dshear1 = [dx1, dy1].*[Y,0]
-dshear2 = [dx2, dy2].*[Y,0]
-
-
-
-
-
-
-
+dist = norm(I1 +M1*u1 - I2 +M2*u2);
 
 end
 
